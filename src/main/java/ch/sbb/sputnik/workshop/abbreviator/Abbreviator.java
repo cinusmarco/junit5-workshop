f073b0ccf727a98d8eaa753736f87ad098faef53
Cinus Marco (IT-SWE-CC1-JV2) <marco.cinus@sbb.ch>
2019-12-05 10:47:42 +0100

    Corrected Abbreviator & added extra example for the extension mechanism

--- src/main/java/ch/sbb/sputnik/workshop/abbreviator/Abbreviator.java
+++ src/main/java/ch/sbb/sputnik/workshop/abbreviator/Abbreviator.java
@@ -4,31 +4,31 @@
 
 package ch.sbb.sputnik.workshop.abbreviator;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
-
 public class Abbreviator {
 
   public String abbreviate(String string) {
-    Pattern p = Pattern.compile("\\w");
-    String[] tokens = string.split("\\w");
 
-    char[] chars = string.substring(1, string.length() - 1).toCharArray();
-    int size = 0;
-    List<String> toAdd = new ArrayList<>();
-    for (char c : chars) {
-      ++size;
-      if (!p.matcher(String.valueOf(c)).find()) {
-        toAdd.add(String.valueOf(c));
+    String[] tokens = string.split("\\W");
+    StringBuilder stringBuilder = new StringBuilder();
+
+    int currentPointer = 0;
+
+    for (String token : tokens) {
+      if (token.length() < 4) {
+        stringBuilder.append(token);
+      } else {
+        int size = token.length() - 2;
+
+        stringBuilder.append(token, 0, 1);
+        stringBuilder.append(size);
+        stringBuilder.append(token.substring(token.length() - 1));
       }
+      currentPointer = currentPointer + token.length();
+      if (string.length() > currentPointer) {
+        stringBuilder.append(string.charAt(currentPointer));
+      }
+      ++currentPointer;
     }
-    StringBuffer stringBuffer = new StringBuffer();
-    stringBuffer.append(string.substring(0, 1));
-    for (String s : toAdd) {
-      stringBuffer.append(s);
-    }
-    stringBuffer.append(string.substring(string.length() - 1));
-    return stringBuffer.toString();
+    return stringBuilder.toString();
   }
 }
