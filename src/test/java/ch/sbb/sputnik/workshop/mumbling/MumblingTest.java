1e1876e6e679b1e9ba7383810089c02c09516e59
e516763 <e516763@sbb.ch>
2019-12-06 11:27:11 +0100

    remove duplicate code in mumbling

--- src/test/java/ch/sbb/sputnik/workshop/mumbling/MumblingTest.java
+++ src/test/java/ch/sbb/sputnik/workshop/mumbling/MumblingTest.java
@@ -5,44 +5,28 @@
 package ch.sbb.sputnik.workshop.mumbling;
 
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MumblingTest {
 
-    @Test
-    public void test1() {
+    @ParameterizedTest
+    @MethodSource("provider")
+    void testStreamsOfArguments(String accumulated, String expected) {
         Mumbling systemUnderTest = new Mumbling();
-        assertThat(systemUnderTest.accum("ZpglnRxqenU"))
-                .isEqualTo("Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+        assertThat(systemUnderTest.accum(accumulated)).isEqualTo(expected);
     }
-
-    @Test
-    public void test2() {
-        Mumbling systemUnderTest = new Mumbling();
-        assertThat(systemUnderTest.accum("NyffsGeyylB"))
-                .isEqualTo("N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
-    }
-
-    @Test
-    public void test3() {
-        Mumbling systemUnderTest = new Mumbling();
-        assertThat(systemUnderTest.accum("EvidjUnokmM"))
-                .isEqualTo("E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
-    }
-
-    @Test
-    public void test4() {
-        Mumbling systemUnderTest = new Mumbling();
-        assertThat(systemUnderTest.accum("MjtkuBovqrU"))
-                .isEqualTo("M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
-    }
-
-    @Test
-    public void test5() {
-        Mumbling systemUnderTest = new Mumbling();
-        assertThat(systemUnderTest.accum("HbideVbxncC"))
-                .isEqualTo("H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
-    }
+    private static Stream<Arguments> provider() {?
+        return Stream.of(
+                Arguments.of("ZpglnRxqenU", "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"),
+                Arguments.of("NyffsGeyylB", "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"),
+                Arguments.of("EvidjUnokmM", "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"),
+                Arguments.of("MjtkuBovqrU", "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"),
+                Arguments.of("HbideVbxncC", "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"));
+    }?
 }
